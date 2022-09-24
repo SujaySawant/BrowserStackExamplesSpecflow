ab5fa89b3a10da458f0a6408646360d05c0dd4fe
Sujay Sawant <sujay.s@browserstack.com>
2022-09-25 04:17:34 +0530

    Removed console output statements.

--- BrowserStackExamplesSpecflow/StepDefinitions/LocalHooks.cs
+++ BrowserStackExamplesSpecflow/StepDefinitions/LocalHooks.cs
@@ -14,15 +14,12 @@ namespace BrowserStackExamplesSpecflow.StepDefinitions
             List<KeyValuePair<string, string>> bsArgs = new List<KeyValuePair<string, string>>();
             bsArgs.Add(new KeyValuePair<string, string>("key", Environment.GetEnvironmentVariable("BROWSERSTACK_ACCESS_KEY") ?? "BROWSERSTACK_ACCESS_KEY"));
             BrowserstackLocalService.Start(bsArgs);
-            Console.WriteLine("Started Local Connection");
-            //dotnet test --filter Category=local
         }
 
         [AfterTestRun]
         public static void AfterTestRun()
         {
             BrowserstackLocalService.Stop();
-            Console.WriteLine("Stopped Local Connection");
         }
     }
 }
